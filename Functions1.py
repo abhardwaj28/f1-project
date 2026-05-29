2e4528334cf356adef69a712c864e53f855ac762
Apoorva Bhardwaj <bhardwajapoorva2848@gmail.com>
2026-05-29 06:13:24 +0530

    Refactor load_session and get_results functions

--- Functions1.py
+++ Functions1.py
@@ -1,25 +1,45 @@
 import fastf1
-def load_session(year, gp, session_type):
+import streamlit as st
 
-    session = fastf1.get_session(
-        year,
-        gp,
-        session_type
+fastf1.Cache.enable_cache(
+    "./cache"
+)
+
+
+@st.cache_resource
+def load_session(
+    year,
+    gp,
+    session_type
+):
+
+    session = (
+        fastf1.get_session(
+            year,
+            gp,
+            session_type
+        )
     )
 
     session.load()
 
     return session
-def get_results(session):
 
-    results = session.results[
-        [
-            "Position",
-            "Abbreviation",
-            "FullName",
-            "TeamName",
-            "Points"
+
+def get_results(
+    session
+):
+
+    results = (
+        session.results[
+            [
+                "Position",
+                "Abbreviation",
+                "FullName",
+                "TeamName",
+                "Points"
+            ]
         ]
-    ]
+    )
 
     return results
